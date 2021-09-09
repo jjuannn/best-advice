bb0f12102eb89b07ebdd19ee5435a8446c3be6c6
juan <juancraft12yt@gmail.com>
2021-09-08 23:09:45 -0300

    update home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,53 @@
+import {
+  Flex,
+  Text,
+  Image,
+  Heading,
+  Box,
+  Button,
+  Link as ChakraLink,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import PageContainer from "components/PageContainer";
+
 export default function Home() {
   return (
-    <p>hola mundo!</p>
-  )
+    <PageContainer>
+      <Flex
+        width={{ sm: "100%", md: "80%" }}
+        flexDirection={{ sm: "column", lg: "row" }}
+        justifyContent="space-evenly"
+        alignItems="center"
+      >
+        <Box textAlign="center" marginX="10px">
+          <Heading color="purple.500" fontSize="50px">
+            Welcome to Best Advice!
+          </Heading>
+          <Text marginTop="10px" fontSize="25px">
+            Get in touch with people around the world to give advices to your
+            problems!
+            <br />
+            <Button
+              verticalAlign="center"
+              colorScheme="purple"
+              color="white"
+              fontWeight="bold"
+              padding="0"
+              margin="20px 0px"
+            >
+              <Link href="/list">
+                <ChakraLink
+                  _hover={{ textDecoration: "none" }}
+                  style={{ display: "block", padding: "1em" }}
+                >
+                  Get started
+                </ChakraLink>
+              </Link>
+            </Button>
+          </Text>
+        </Box>
+        <Image width="250px" src="/svg/chat.svg" />
+      </Flex>
+    </PageContainer>
+  );
 }
