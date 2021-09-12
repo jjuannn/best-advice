7a75a66583da890ee67e171906806da6367829ad
juan <juancraft12yt@gmail.com>
2021-09-12 02:18:50 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,18 @@ import { ChakraProvider } from "@chakra-ui/react";
 import AppLayout from "/components/Layout";
 import { customTheme } from "/styles/config";
 import Head from "/components/Head/index";
+import UserContextProvider from "context/user";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={customTheme}>
-      <Head />
-      <AppLayout>
-        <Component {...pageProps} />
-      </AppLayout>
-    </ChakraProvider>
+    <UserContextProvider>
+      <ChakraProvider theme={customTheme}>
+        <Head />
+        <AppLayout>
+          <Component {...pageProps} />
+        </AppLayout>
+      </ChakraProvider>
+    </UserContextProvider>
   );
 }
 
