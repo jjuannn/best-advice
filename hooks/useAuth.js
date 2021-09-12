400e868bf1fad629425d1f270ab08f01fbaec2a1
juan <juancraft12yt@gmail.com>
2021-09-12 02:15:43 -0300

    create useAuth hook

---
+++ hooks/useAuth.js
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+import { userContext } from "context/user";
+import {
+  registerWithEmailAndPassword,
+  logout as logoutService,
+} from "/firebase/user.js";
+import { useRouter } from "next/router";
+
+export default function useAuth() {
+  const { userValues, userDispatch } = useContext(userContext);
+  const router = useRouter();
+  const register = async ({ email, password }) => {
+    try {
+      const data = await registerWithEmailAndPassword(email, password);
+      userDispatch({ type: "USER_AUTH", payload: data });
+      router.push("/");
+    } catch (err) {}
+  };
+
+  const logout = async () => {
+    await logoutService();
+    userDispatch({ type: "USER_LOGOUT" });
+  };
+
+  return {
+    register,
+    logout,
+    userValues,
+  };
+}
