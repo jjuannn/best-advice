44fd19f2b0848f06876a05e8664ca27c7a595656
juan <juancraft12yt@gmail.com>
2021-09-17 23:40:26 -0300

    update post item component

--- components/Post/Item/index.js
+++ components/Post/Item/index.js
@@ -1,5 +1,10 @@
-import { Box, Text, Heading, Badge } from "@chakra-ui/react";
+import { Box, Text, Heading, Badge, Button } from "@chakra-ui/react";
 import useAuth from "hooks/useAuth";
+import usePosts from "hooks/usePosts";
+import Link from "next/link";
+import { useEffect } from "react";
+import { FaRegTrashAlt } from "react-icons/fa";
+import router from "next/router";
 
 export default function PostItem({
   author,
@@ -11,31 +16,62 @@ export default function PostItem({
   author_id,
 }) {
   const { userValues } = useAuth();
+  const {
+    fDeletePost,
+    deletePostFailure,
+    deletePostLoading,
+    deletePostSuccess,
+  } = usePosts();
+
+  useEffect(() => {
+    if (deletePostSuccess) {
+      router.push("/list");
+    }
+  }, [deletePostSuccess]);
 
   return (
     <>
       <Box
+        _hover={{ cursor: "pointer" }}
         bg="purple.400"
         width="100%"
         marginBottom="15px"
         padding="30px"
         borderRadius="md"
+        color="white"
       >
         <Box>
-          <Text color="white">
+          <Text>
             #1 made by {author} - {createdAt} {"  "}
           </Text>
         </Box>
         <Box>
-          <Heading color="white">{title}</Heading>
+          <Heading>{title}</Heading>
           <Text isTruncated marginTop="10px" color="white">
             {text}
           </Text>
           <Badge colorScheme="purple"># {topic}</Badge>
         </Box>
-        {author_id === userValues?.uid
-          ? "TIENEN LA MISMA ID"
-          : "NO TIENEN LA MISMA ID "}
+        <Link href={`/detail/${id}`}>
+          <Button marginTop="30px" marginRight="5px" colorScheme="whiteAlpha">
+            Detail
+          </Button>
+        </Link>
+        {author_id === userValues?.uid ? (
+          <Button
+            isLoading={deletePostLoading}
+            onClick={() => {
+              fDeletePost(id);
+            }}
+            marginTop="30px"
+            colorScheme="red"
+            leftIcon={<FaRegTrashAlt />}
+          >
+            Delete
+          </Button>
+        ) : (
+          ""
+        )}
       </Box>
     </>
   );
